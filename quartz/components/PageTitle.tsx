f85fce6d291c844024948d06f7090a0e1297e7b6
wandering meditations <ibrareadsg@gmail.com>
2024-10-09 19:30:29 -0400

    Quartz sync: Oct 9, 2024, 7:30 PM

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -8,7 +8,7 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
   const baseDir = pathToRoot(fileData.slug!)
   return (
     <h2 class={classNames(displayClass, "page-title")}>
-      <a href={baseDir}>{title}</a>
+      <a href={baseDir}>Wandering Meditations</a>
     </h2>
   )
 }
